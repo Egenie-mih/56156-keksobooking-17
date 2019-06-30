cedd8cdc181e758d40877112f6842a782b3151fc
Evgeniya Mikhaylova <evgeniya.mikhaylova@omnigon.com>
2019-06-30 15:07:38 +0300

    Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -5,10 +5,18 @@ var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var PIN_Y_MIN = 130;
 var PIN_Y_MAX = 630;
 var PIN_WIDTH = 50;
+var MAIN_PIN_HEIGHT = 80;
+
 var map = document.querySelector('.map');
 var pinList = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapWidth = map.offsetWidth;
+var mapFaded = document.querySelector('.map--faded');
+var mapFilters = document.querySelector('.map__filters');
+var mainPin = document.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var formFieldsets = adForm.querySelectorAll('fieldset');
+var addressInput = adForm.querySelector('input[name="address"]');
 
 function getRandomInteger(array) {
   return Math.floor(Math.random() * (array.length - 1));
@@ -17,6 +25,26 @@ function getRandomInteger(array) {
 function getRandomIntegerInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+var toggleDisabledAttribute = function (array, disabledValue) {
+  for (var i = 0; i < array.length; i++) {
+    array[i].disabled = disabledValue;
+  }
+};
+
+var loadPage = function () {
+  toggleDisabledAttribute(formFieldsets, true);
+  toggleDisabledAttribute(mapFilters, true);
+  addressInput.value = String(mainPin.offsetLeft) + ', ' + String(mainPin.offsetTop);
+};
+
+var activatePage = function () {
+  mapFaded.classList.remove('map--faded');
+  pinList.appendChild(pinElement);
+  adForm.classList.remove('ad-form--disabled');
+  toggleDisabledAttribute(formFieldsets, false);
+  toggleDisabledAttribute(mapFilters, false);
+};
 
 var createOffersPins = function () {
   var allPins = [];
@@ -70,5 +98,15 @@ function setPins(offersData) {
 
 var pinsList = createOffersPins();
 var pinElement = setPins(pinsList);
-pinList.appendChild(pinElement);
-map.classList.remove('map--faded');
+
+mainPin.addEventListener('click', function () {
+  activatePage();
+});
+
+mainPin.addEventListener('mouseup', function () {
+  activatePage();
+  addressInput.value = String(mainPin.offsetLeft + Math.round(PIN_WIDTH / 2)) + ', ' + String(mainPin.offsetTop + MAIN_PIN_HEIGHT);
+});
+
+loadPage();
+
